5ad4d06ed1be3fe12e9136a70c0665bf8bf7d503
FedericoPalaz <federico.palazzini@studenti.unitn.it>
2016-12-03 18:28:54 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ function avanti(s1,s2)
 {
    document.getElementById(s1).style.display="none";
    document.getElementById(s2).style.display="block";
-   document.getElementById("txt_"+s1).style.display="none";
-   document.getElementById("txt_"+s2).style.display="block";
+   document.getElementById("txt_"+s1).style.visibility="hidden";
+   document.getElementById("txt_"+s2).style.visibility="visible";
 }
 
 function indietro(s1,s2)
 {
    document.getElementById(s1).style.display="none";
    document.getElementById(s2).style.display="block";
-   document.getElementById("txt_"+s1).style.display="none";
-   document.getElementById("txt_"+s2).style.display="block";
+   document.getElementById("txt_"+s1).style.visibility="hidden";
+   document.getElementById("txt_"+s2).style.visibility="visible";
 }
