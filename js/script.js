b64deaf79d62aeb262b3819f90f92c53a50a5560
FedericoPalaz <federico.palazzini@studenti.unitn.it>
2016-12-04 12:48:58 +0100

    update aggiunto qualche funzione

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-function avanti(s1,s2)
+function apri(s)
 {
-   document.getElementById(s1).style.display="none";
-   document.getElementById(s2).style.display="block";
-   document.getElementById("txt_"+s1).style.visibility="hidden";
-   document.getElementById("txt_"+s2).style.visibility="visible";
-}
-
-function indietro(s1,s2)
-{
-   document.getElementById(s1).style.display="none";
-   document.getElementById(s2).style.display="block";
-   document.getElementById("txt_"+s1).style.visibility="hidden";
-   document.getElementById("txt_"+s2).style.visibility="visible";
+   var x=document.getElementById(s);
+    if(x.style.display=="none")
+        {
+            x.style.display="block"
+        }
+    else
+        {
+            x.style.display="none"
+        }
 }
